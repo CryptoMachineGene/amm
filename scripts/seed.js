be012d93d10009592a3503aa37824a88a44d83d6
cplKlegg <e8mcgrath@gmail.com>
2025-07-01 00:12:08 -0400

    Seed AMM with minor issues to be resolved

--- scripts/seed.js
+++ scripts/seed.js
@@ -25,6 +25,124 @@ async function main() {
   const investor3 = accounts[3]
   const investor4 = accounts[4]
 
+  // Fetch Network
+  const { chainId } = await ethers.provider.getNetwork()
+
+  console.log(`Fetching token and transferring to accounts... \n`)
+
+  // Fetch Dapp Token
+  const dapp = await ethers.getContractAt('Token', config[chainId].dapp.address)
+  console.log(`Dapp Token fetched: ${dapp.address} \n`)
+
+  // Fetch USD Token
+  const usd = await ethers.getContractAt('Token', config[chainId].usd.address)
+  console.log(`USD Token fetched: ${usd.address} \n`)
+
+
+  ///////////////////////////////////////////////////////////////
+  // Distribute Tokens to Investors
+  //
+
+  let transaction
+
+  // Send dapp tokens to investor 1
+  transaction = await dapp.connect(deployer).transfer(investor1.address, tokens(10))
+  await transaction.wait()
+
+  // Send usd tokens to investor 2
+  transaction = await usd.connect(deployer).transfer(investor2.address, tokens(10))
+  await transaction.wait()
+
+  // Send dapp tokens to investor 3
+  transaction = await dapp.connect(deployer).transfer(investor3.address, tokens(10))
+  await transaction.wait()
+
+  // Send usd tokens to investor 4
+  transaction = await usd.connect(deployer).transfer(investor4.address, tokens(10))
+  await transaction.wait()
+  
+
+  ///////////////////////////////////////////////////////////////
+  // Adding Liquidity
+  //
+
+  let amount = tokens(100)
+
+  console.log(`Fetching AMM... \n`)
+
+  // Fetch AMM
+  const amm = await ethers.getContractAt('AMM', config[chainId].amm.address)
+  console.log(`AMM fetched: ${amm.address} \n`)
+
+  transaction = await dapp.connect(deployer).approve(amm.address, amount)
+  await transaction.wait()
+
+  transaction = await usd.connect(deployer).approve(amm.address, amount)
+  await transaction.wait()
+
+  // Deployer add liquidity
+  console.log(`Adding liquidity... \n`)
+  transaction = await amm.connect(deployer).addLiquidity(amount, amount)
+  await transaction.wait()
+
+  ///////////////////////////////////////////////////////////////
+  // Investor 1 Swaps: Dapp --> USD
+  //
+
+  console.log(`Investor 1 Swaps... \n`)
+
+  // Investor approves all tokens
+  transaction = await dapp.connect(investor1).approve(amm.address, tokens(10))
+  await transaction.wait()
+
+  // Investor swaps 1 token
+  transaction = await amm.connect(investor1).swapToken1(tokens(1))
+  await transaction.wait()
+
+  ///////////////////////////////////////////////////////////////
+  // Investor 2 Swaps: USD --> Dapp
+  //
+
+  console.log(`Investor 2 Swaps... \n`)
+
+  // Investor approves all tokens
+  transaction = await dapp.connect(investor2).approve(amm.address, tokens(10))
+  await transaction.wait()
+
+  // Investor swaps 1 token
+  transaction = await amm.connect(investor2).swapToken2(tokens(1))
+  await transaction.wait()
+
+  ///////////////////////////////////////////////////////////////
+  // Investor 3 Swaps: Dapp --> USD
+  //
+
+  console.log(`Investor 3 Swaps... \n`)
+
+  // Investor approves all tokens
+  transaction = await dapp.connect(investor3).approve(amm.address, tokens(10))
+  await transaction.wait()
+
+  // Investor swaps all 10 tokens
+  transaction = await amm.connect(investor3).swapToken1(tokens(10))
+  await transaction.wait()
+
+  ///////////////////////////////////////////////////////////////
+  // Investor 4 Swaps: USD --> Dapp
+  //
+
+  console.log(`Investor 4 Swaps... \n`)
+
+  // Investor approves all tokens
+  transaction = await dapp.connect(investor4).approve(amm.address, tokens(10))
+  await transaction.wait()
+
+  // Investor swaps 5 tokens
+  transaction = await amm.connect(investor4).swapToken2(tokens(5))
+  await transaction.wait()
+
+  console.log(`Finished. \n`)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
