78ba098ffc16cc7b38b08e39407b332bbc91a6dd
cplKlegg <e8mcgrath@gmail.com>
2025-08-08 00:53:39 -0400

    Adds Chart page

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -23,8 +23,6 @@ const Charts = () => {
     
     const chart = useSelector(chartSelector)
 
-    const swaps = useSelector(state => state.amm.swaps)
-    
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -35,55 +33,62 @@ const Charts = () => {
 
     return (
        <div>
-          {chart}
-        <Chart 
+         {provider && amm ? (
+            <div>
+               <Chart 
             type="line"
             options={options}
-            series={series}
+            series={chart ? chart.series : series}
             width="100%"
             height="100%"
-        />            
-          <hr />
+               />   
 
-          <Table striped bordered hover>
-               <thead>
-                   <tr>
-                       <th>Transaction Hash</th>
-                       <th>Token Give</th>
-                       <th>Amount Give</th>
-                       <th>Token Get</th>
-                       <th>Amount Get</th>
-                       <th>User</th>
-                       <th>Time</th>
-                   </tr>
-               </thead>
-               <tbody>
-                   {swaps && swaps.map((swap, index) => (
-                        <tr key={index}>
-                           <td>{swap.hash.slice(0, 5) + '...' + swap.hash.slice(61, 66)}</td>
-                           <td>{swap.args.tokenGive === tokens[0].address ? symbols[0] : symbols[1]}</td>
-                           <td>{ethers.utils.formatUnits(swap.args.tokenGiveAmount.toString(), 'ether')}</td>
-                           <td>{swap.args.tokenGet === tokens[0].address ? symbols[0] : symbols[1]}</td>
-                           <td>{ethers.utils.formatUnits(swap.args.tokenGetAmount.toString(), 'ether')}</td>
-                           <td>{swap.args.user.slice(0, 5) + '...' + swap.args.user.slice(38, 42)}</td>
-                           <td>{
-                            new Date(Number(swap.args.timestamp.toString() + '000'))
-                            .toLocaleDateString(
-                                undefined,
-                                {
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric',
-                                    hour: 'numeric',
-                                    minute: 'numeric',
-                                    second: 'numeric'
-                                }
-                            )
-                           }</td> 
+               <hr />
+
+              <Table striped bordered hover>
+                   <thead>
+                       <tr>
+                           <th>Transaction Hash</th>
+                           <th>Token Give</th>
+                           <th>Amount Give</th>
+                           <th>Token Get</th>
+                           <th>Amount Get</th>
+                           <th>User</th>
+                           <th>Time</th>
                        </tr>
-                    ))}               
-               </tbody>
-            </Table>  
+                   </thead>
+                   <tbody>
+                       {chart.swaps && chart.swaps.map((swap, index) => (
+                            <tr key={index}>
+                               <td>{swap.hash.slice(0, 5) + '...' + swap.hash.slice(61, 66)}</td>
+                               <td>{swap.args.tokenGive === tokens[0].address ? symbols[0] : symbols[1]}</td>
+                               <td>{ethers.utils.formatUnits(swap.args.tokenGiveAmount.toString(), 'ether')}</td>
+                               <td>{swap.args.tokenGet === tokens[0].address ? symbols[0] : symbols[1]}</td>
+                               <td>{ethers.utils.formatUnits(swap.args.tokenGetAmount.toString(), 'ether')}</td>
+                               <td>{swap.args.user.slice(0, 5) + '...' + swap.args.user.slice(38, 42)}</td>
+                               <td>{
+                                new Date(Number(swap.args.timestamp.toString() + '000'))
+                                .toLocaleDateString(
+                                    undefined,
+                                    {
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: 'numeric',
+                                        hour: 'numeric',
+                                        minute: 'numeric',
+                                        second: 'numeric'
+                                    }
+                                )
+                               }</td> 
+                           </tr>
+                        ))}               
+                   </tbody>
+                </Table>  
+            </div>
+         ) : (
+            <Loading/>
+         )}
+        
        </div>
        
     );
